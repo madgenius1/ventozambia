251ec321a3afc2cb7f32d8d0fc8d7c6fac151ed5
MadGenius <nickjuma@outlook.com>
2025-08-15 11:12:35 +0300

    modified:   src/app/ui-items/landing/Hero.tsx

--- src/app/ui-items/landing/Hero.tsx
+++ src/app/ui-items/landing/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import React from "react";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
