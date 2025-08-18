24c54e3a61957f85f872d59cab02859e6c1765c0
MadGenius <nickjuma@outlook.com>
2025-08-19 02:28:27 +0300

    modified:   src/app/ui-items/landing/CallToAction.tsx, deleted BrandSection, and deleted ProductCard

--- src/app/ui-items/landing/Affiliations.tsx
+++ src/app/ui-items/landing/Affiliations.tsx
@@ -1,22 +1,22 @@
 
 
 
-export default function CallToAction() {
+export default function Affiliiations() {
     return (
-        <main className="bg-neutral-50">
+        <main className="bg-white">
             <div className="lg:p-8 md:p-6 p-4">
                 <div className="max-w-screen-xl mx-auto p-4">
                     <h3 className="text-start text-neutral-800 dark:text-neutral-200 font-bold lg:text-4xl text-2xl">
-                        Contact Us
+                        Distributor Affiliations
                     </h3>
                     <p className="lg:text-xl text-sm pt-4 text-neutral-700 font-medium leading-relaxed max-w-3xl">
-                        Reach out to us.<br />
-                        We would love to hear from you and work with you.
+                        We are part of a great team.<br />
+                        We partner with other businesses and industries to streamline the delivery of goods and services to you.
                     </p>
                     <hr className="my-10 border-gray-400" />
                     <div className="my-10 flex items-center justify-center">
                         <h3 className="text-lg font-semibold text-neutral-950 text-center">
-                            Contact items for direct communication: Emails, Phone Numbers for calls, and WhatsApp direct message.
+                            List of affiliations such as chamber of commerce, part of a distibution alliance etc.
                         </h3>
                     </div>
                 </div>
